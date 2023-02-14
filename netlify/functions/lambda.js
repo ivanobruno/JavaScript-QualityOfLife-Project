b39ea55a9a81fb217ebc1294c44779970204c037
Ivano Bruno <ivanobruno1@gmail.com>
2023-02-14 03:12:48 +0100

    Update lambda.js

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -1,10 +1,4 @@
-import axios from "axios";
-import { cityName } from "../../src/js/dom-elemets";
-
 export async function handler(event) {
-    const API_URL = process.env.API_URL;
-
-    const response = await axios.get(API_URL + `slug:${cityName}/scores/`);
     const data = await response.json();
 
     const pass = (body) => {
